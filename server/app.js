2a103efa111dd6fd8e50fcd59c6bc33ecddabf6a
fernando <fernandordyansyah@gmail.com>
2023-12-15 11:34:15 +0700

    config setup

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 const express = require('express')
 const router = require('./routers')
 const app = express()
-const port = 3000
+// const port = 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
@@ -15,6 +15,8 @@ app.use(express.json())
 
 app.use(router)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+module.exports = app
